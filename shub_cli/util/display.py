9908cb850872552377982c1508a0ed7c89c1dbdd
Victor Martinez <vcrmartinez@gmail.com>
2016-09-25 21:50:01 -0300

    Implement display util

---
+++ shub_cli/util/display.py
@@ -0,0 +1,27 @@
+def display(job):
+    if job:
+        info = job.info
+        print('')
+        print('##############################')
+        print('Job: {}'.format(info['id']))
+        print('##############################')
+        print('Started At: {}'.format(info['started_time']))
+        print('Spider: {}'.format(info['spider']))
+        print('Items Scraped: {}'.format(info['items_scraped']))
+        print('State: {}'.format(info['state']))
+        print('Close Reason: {}'.format(info['close_reason']))
+        print('Errors: {}'.format(info['errors_count']))
+        print('Spider Args: {}'.format(info['spider_args']))
+        print('Tags: {}'.format(info['tags']))
+        print('Version: {}'.format(info['version']))
+    else:
+        print('')
+        print('Job not found')
+
+
+def display_jobs(jobs):
+    if not jobs:
+        print('Jobs not found')
+
+    for job in jobs:
+        display(job)
