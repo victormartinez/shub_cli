122e6a7f29364ec4bd0dcfa71d4b624423563305
Victor Martinez <vcrmartinez@gmail.com>
2016-09-28 20:09:37 -0300

    Fix tests of parse module

--- tests/util/test_parse.py
+++ tests/util/test_parse.py
@@ -81,13 +81,18 @@ def test_parse_list_to_string(tag_list, expected):
     assert tags_string == expected
 
 
-@pytest.mark.parametrize("tag, lacks, spider, state, expected", [
+@pytest.mark.parametrize("tag, lacks, spider, state, count, expected", [
     ('production', None, None, None, None, {'has_tag': 'production', 'count': 10}),
     ('production', 'development', None, None, None, {'has_tag': 'production', 'lacks_tag': 'development', 'count': 10}),
     ('production', 'development', 'my-spider', None, None, {'has_tag': 'production', 'lacks_tag': 'development', 'spider': 'my-spider', 'count': 10}),
     ('production', 'development', 'my-spider', 'finished', None, {'has_tag': 'production', 'lacks_tag': 'development', 'spider': 'my-spider', 'state': 'finished', 'count': 10}),
-    ('production', 'development', 'my-spider', 'finished', '30', {'has_tag': 'production', 'lacks_tag': 'development', 'spider': 'my-spider', 'state': 'finished', 'count': 30}),
+    ('production', 'development', 'my-spider', 'finished', 30, {'has_tag': 'production', 'lacks_tag': 'development', 'spider': 'my-spider', 'state': 'finished', 'count': 30}),
+    (None, None, None, None, 30, {'count': 30}),
+    (None, None, None, 'finished', 30, {'state': 'finished', 'count': 30}),
+    (None, None, 'my-spider', 'finished', 30, {'state': 'finished', 'count': 30, 'spider': 'my-spider'}),
+    (None, 'development', 'my-spider', 'finished', 30, {'state': 'finished', 'lacks_tag': 'development', 'count': 30, 'spider': 'my-spider'}),
 ])
 def test_parse_options(tag, lacks, spider, state, count, expected):
     options = parse_options(tag, lacks, spider, state, count)
+    print(options)
     assert options == expected
