0b158676c20715d299f1c32f171377cd70625b5b
Victor Martinez <vcrmartinez@gmail.com>
2016-10-05 20:31:26 -0300

    Refactor get_job_main_info module

--- shub_cli/util/parse.py
+++ shub_cli/util/parse.py
@@ -32,8 +32,6 @@ def parse_list_to_string(tags):
     :param tags: A list of tags
     :return: A string with tags separated by comma
     """
-    if len(tags) == 1:
-        return tags[0]
     return ', '.join(tags)
 
 
@@ -45,43 +43,15 @@ def get_job_main_info(job):
     :return: A dictionary
     """
     info = job.info
-    main_info = {}
-    main_info.update({'id': job.id})
-    main_info.update({'spider': info['spider']})
-
-    state = ''
-    if 'state' in info.keys():
-        state = info['state']
-
-    items_scraped = ''
-    if 'items_scraped' in info.keys():
-        items_scraped = info['items_scraped']
-
-    errors_count = ''
-    if 'errors_count' in info.keys():
-        errors_count = info['errors_count']
-
-    tags = ''
-    if 'tags' in info.keys():
-        tags = parse_list_to_string(info['tags'])
-
-    version = ''
-    if 'version' in info.keys():
-        version = info['version']
-
-    started = ''
-    if 'started_time' in info.keys():
-        started = info['started_time']
-
-    close = ''
-    if 'close_reason' in info.keys():
-        close = info['close_reason']
-
-    main_info.update({'state': state})
-    main_info.update({'items_scraped': items_scraped})
-    main_info.update({'errors_count': errors_count})
-    main_info.update({'tags': tags})
-    main_info.update({'version': version})
-    main_info.update({'started_time': started})
-    main_info.update({'close_reason': close})
+    main_info = {
+        'id': job.id,
+        'spider': info.get('spider'),
+        'state': info.get('state', ''),
+        'items_scraped': info.get('items_scraped', ''),
+        'errors_count': info.get('errors_count',  ''),
+        'tags': ', '.join(info.get('tags', [])),
+        'version': info.get('version', ''),
+        'started_time': info.get('started_time', ''),
+        'close_reason': info.get('close_reason', ''),
+    }
     return main_info
