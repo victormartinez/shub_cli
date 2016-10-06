fe308f18a4fb16557a16ce392216e3844cc1696a
Victor Martinez <vcrmartinez@gmail.com>
2016-10-05 21:29:14 -0300

    Add comment to get_spiders function

--- shub_cli/commands/spider.py
+++ shub_cli/commands/spider.py
@@ -2,4 +2,11 @@
 
 
 def get_spiders(conn, project):
+    """
+    Get a list of spiders from ScrapingHub.
+
+    :param conn: Valid connection object
+    :param project: Project id
+    :return: A List of dicts provided by ScrapingHub
+    """
     return list(conn[project].spiders())
